157200f5f9fa0763095de31d74fb09fece4a187d
root <calebkipyatich@gmail.com>
2020-01-23 01:49:45 +0300

    Implement firebase authentication

--- app/src/main/java/com/chats/message/LoginActivity.java
+++ app/src/main/java/com/chats/message/LoginActivity.java
@@ -1,20 +1,104 @@
 package com.chats.message;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = LoginActivity.class.getName();
+
+    // widgets
+    private EditText userEmail, userPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userEmail = findViewById(R.id.user_email);
+        userPassword = findViewById(R.id.user_password);
+        progressBar = findViewById(R.id.progress_bar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        setUpFirebaseListener();
+    }
+
+    private void setUpFirebaseListener() {
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    openChatScreen();
+                    Toast.makeText(LoginActivity.this, "Signed in with " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                }else {
+                    Log.d(TAG, "onAuthStateChanged: user signed out");
+                }
+            }
+        };
     }
 
     public void logIn(View view) {
+
+        showProgressBar();
+        hideSoftKeyboard();
+
+        String email = userEmail.getText().toString().trim();
+        String password = userPassword.getText().toString().trim();
+
+        // check email and password fields are not empty before signing in user
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful()){
+                        hideProgressBar();
+                    }
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d(TAG, "onFailure: Error " + e);
+                    Toast.makeText(LoginActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                    hideProgressBar();
+                }
+            });
+        } else {
+            Toast.makeText(this, "Fill out the fields", Toast.LENGTH_SHORT).show();
+            hideProgressBar();
+        }
+    }
+
+    private void openChatScreen() {
+        Intent intent = new Intent(this, ChatActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
     public void openSignUpActivity(View view) {
@@ -22,5 +106,33 @@ public class LoginActivity extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
+    }
+
+    private void showProgressBar(){
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar(){
+        if (progressBar.getVisibility() == View.VISIBLE){
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (authStateListener != null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
--- app/src/main/java/com/chats/message/SignUpActivity.java
+++ app/src/main/java/com/chats/message/SignUpActivity.java
@@ -1,19 +1,31 @@
 package com.chats.message;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    public static final String TAG = SignUpActivity.class.getName();
+
     // widgets
     private EditText userName, userEmail, userPassword, confirmPassword;
+    private ProgressBar progressBar;
 
     // vars
     FirebaseAuth firebaseAuth;
@@ -27,20 +39,85 @@ public class SignUpActivity extends AppCompatActivity {
         userEmail = findViewById(R.id.user_email);
         userPassword = findViewById(R.id.user_password);
         confirmPassword = findViewById(R.id.user_confirm_password);
+        progressBar = findViewById(R.id.progress_bar);
 
         firebaseAuth = FirebaseAuth.getInstance();
     }
 
     public void signUp(View view) {
 
+        showProgressBar();
+        hideSoftKeyboard();
+
         String email = userEmail.getText().toString().trim();
         String password = userPassword.getText().toString().trim();
         String confirmationPassword = confirmPassword.getText().toString().trim();
 
         if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(confirmationPassword)){
-            // sign up user
+            if (password.equals(confirmationPassword)){
+                boolean isValidDomain = confirmEmailDomain(email);
+                if (isValidDomain) {
+                    // sign up user
+                    signUpUser(email, password);
+                } else {
+                    Toast.makeText(this, "Register with valid email domain", Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(this, "Passwords do not Match", Toast.LENGTH_SHORT).show();
+            }
         } else {
-            Toast.makeText(this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Fill out all the fields", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    // sign up user
+    private void signUpUser(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                hideProgressBar();
+                if (task.isSuccessful()){
+                    Toast.makeText(SignUpActivity.this, "Signed Up", Toast.LENGTH_SHORT).show();
+                    firebaseAuth.signOut();
+                    redirectToLoginScreen();
+                }else {
+                    Toast.makeText(SignUpActivity.this, "Unable to register", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                hideProgressBar();
+                Log.d(TAG, "onFailure: Error: " + e);
+                Toast.makeText(SignUpActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                firebaseAuth.signOut();
+                redirectToLoginScreen();
+            }
+        });
+    }
+
+    private boolean confirmEmailDomain(String email){
+        String domain = email.substring(email.indexOf("@") + 1).toLowerCase();
+        return domain.equals("gmail.com");
+    }
+
+    private void redirectToLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void showProgressBar(){
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar(){
+        if (progressBar.getVisibility() == View.VISIBLE){
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
